e8a17ec152d886a7281b10af4a84bc36d3a6ff7c
andersentess <andersentess@gmail.com>
2023-11-14 16:20:09 -0700

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://www.linkedin.com/in/tess-andersen/" target="_blank">
-          Tess Andersen
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/andersentess/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://www.linkedin.com/in/tess-andersen/" target="_blank">
+            Tess Andersen
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/andersentess/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
